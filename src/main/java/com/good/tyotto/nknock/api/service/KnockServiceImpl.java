eb6d50b4f400a1ed280ba30dd6fe033d7b6a6ba1
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-06-10 23:26:49 +0900

    ノックAPIのcontrollerと単体テスト実装

--- src/main/java/com/good/tyotto/nknock/api/service/KnockServiceImpl.java
+++ src/main/java/com/good/tyotto/nknock/api/service/KnockServiceImpl.java
@@ -2,7 +2,9 @@ package com.good.tyotto.nknock.api.service;
 
 import com.good.tyotto.nknock.api.domain.Knock;
 import com.good.tyotto.nknock.api.repository.KnockRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KnockServiceImpl implements KnockService {
 
     private final KnockRepository knockRepository;
---
+++ src/test/java/com/good/tyotto/nknock/api/controller/KnockRestControllerTest.java
@@ -0,0 +1,49 @@
+package com.good.tyotto.nknock.api.controller;
+
+import com.good.tyotto.nknock.api.domain.Knock;
+import com.good.tyotto.nknock.api.domain.User;
+import com.good.tyotto.nknock.api.service.KnockService;
+import com.good.tyotto.nknock.api.util.UnitTestUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class KnockRestControllerTest {
+
+    @Mock
+    private KnockService knockService;
+
+    @InjectMocks
+    private  KnockRestController target;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(target).build();
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        Knock input = new Knock();
+        Mockito.doNothing().when(knockService).add(ArgumentMatchers.any(Knock.class));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/knock")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(UnitTestUtil.entity2JsonText(new Knock())));
+
+        Mockito.verify(knockService, Mockito.times(1)).add(ArgumentMatchers.any(Knock.class));
+    }
+}
