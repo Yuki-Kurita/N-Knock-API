8987ae56b2fa18611cccf54c73fc434fb53c77c7
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-06-15 23:19:30 +0900

    ノックリストの編集&削除のController実装&テスト実装

--- src/main/java/com/good/tyotto/nknock/api/controller/KnockListRestController.java
+++ src/main/java/com/good/tyotto/nknock/api/controller/KnockListRestController.java
@@ -8,6 +8,8 @@ import com.good.tyotto.nknock.api.service.KnockListService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,10 @@ public class KnockListRestController {
         this.service = service;
     }
 
+    /**
+     * ノックリスト全件取得
+     * @return 削除されていないノックリスト
+     */
     @CrossOrigin(origins = {"http://localhost:8081"})
     @GetMapping(path = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
     public KnockListGroup findAll() {
@@ -43,16 +49,44 @@ public class KnockListRestController {
         return this.service.find(selector);
     }
 
-
+    /**
+     * タグ全件取得
+     * @return 削除されていないタグ
+     */
     @CrossOrigin(origins = {"http://localhost:8081"})
     @GetMapping(path = "/tags", produces = MediaType.APPLICATION_JSON_VALUE)
     public TagList findTag() {
         return this.service.findTag();
     }
 
+    /**
+     * ノックリストを追加
+     * @param knockList 追加するノックリスト
+     */
     @CrossOrigin(origins = {"http://localhost:8081"})
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public void add(@RequestBody KnockList knockList) {
         this.service.add(knockList);
     }
+
+    /**
+     * ノックリストIDにマッチするノックリストを更新
+     * @param knockList 更新するノックリスト
+     */
+    @CrossOrigin(origins = {"http://localhost:8081"})
+    @PatchMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void set(@RequestBody KnockList knockList) {
+        this.service.set(knockList);
+    }
+
+    /**
+     * ノックリストIDにマッチするノックリストを論理削除
+     * @param knockListId 削除対象のノックリストID
+     */
+    @CrossOrigin(origins = {"http://localhost:8081"})
+    @PatchMapping(path = "/{knockListId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void remove(@PathVariable int knockListId) {
+        this.service.remove(knockListId);
+    }
+
 }
--- src/test/java/com/good/tyotto/nknock/api/controller/KnockListRestControllerTest.java
+++ src/test/java/com/good/tyotto/nknock/api/controller/KnockListRestControllerTest.java
@@ -1,16 +1,19 @@
 package com.good.tyotto.nknock.api.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.good.tyotto.nknock.api.domain.KnockList;
 import com.good.tyotto.nknock.api.domain.KnockListGroup;
 import com.good.tyotto.nknock.api.service.KnockListService;
 import com.good.tyotto.nknock.api.util.UnitTestUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -48,6 +51,36 @@ public class KnockListRestControllerTest {
                 .andReturn();
         assertEquals(UnitTestUtil.entity2JsonText(findResult), result.getResponse().getContentAsString());
         Mockito.verify(knockListService, Mockito.times(1)).findAll();
+    }
+
+    @Test void testAdd() throws Exception {
+        Mockito.doNothing().when(knockListService).add(ArgumentMatchers.any(KnockList.class));
+
+       mockMvc.perform(MockMvcRequestBuilders
+               .post("/knocklist/")
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(UnitTestUtil.entity2JsonText(new KnockList())));
+
+       Mockito.verify(knockListService, Mockito.times(1)).add(ArgumentMatchers.any(KnockList.class));
+    }
+
+    @Test void testSet() throws Exception {
+        Mockito.doNothing().when(knockListService).set(ArgumentMatchers.any(KnockList.class));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .patch("/knocklist/")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(UnitTestUtil.entity2JsonText((new KnockList()))));
+
+       Mockito.verify(knockListService, Mockito.times(1)).set(ArgumentMatchers.any(KnockList.class));
+    }
+
+    @Test void testRemove() throws Exception {
+        int knockListId = 1;
+        Mockito.doNothing().when(knockListService).remove(knockListId);
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/knocklist/1/"));
+        Mockito.verify(knockListService, Mockito.times(1)).remove(knockListId);
     }
 
 }
