11b53209334fe3af86d8bb1096799a044e792c4e
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-06-17 21:28:16 +0900

    Knock API ControllerとTestの実装

--- src/test/java/com/good/tyotto/nknock/api/controller/KnockRestControllerTest.java
+++ src/test/java/com/good/tyotto/nknock/api/controller/KnockRestControllerTest.java
@@ -1,10 +1,14 @@
 package com.good.tyotto.nknock.api.controller;
 
+import static org.junit.Assert.assertEquals;
 import com.good.tyotto.nknock.api.domain.Knock;
+import com.good.tyotto.nknock.api.domain.KnockGroup;
+import com.good.tyotto.nknock.api.domain.KnockSelector;
 import com.good.tyotto.nknock.api.service.KnockService;
 import com.good.tyotto.nknock.api.util.UnitTestUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -12,7 +16,9 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 public class KnockRestControllerTest {
@@ -32,6 +38,24 @@ public class KnockRestControllerTest {
     }
 
     @Test
+    public void testFind() throws  Exception {
+        ArgumentMatcher<KnockSelector> matcher = argument -> {
+            assertEquals(0, argument.getKnockListId());
+            return true;
+        };
+        KnockGroup findResult = new KnockGroup();
+        Mockito.doReturn(findResult).when(knockService).find(Mockito.argThat(matcher));
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                .get("/knock/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        assertEquals(UnitTestUtil.entity2JsonText(findResult), result.getResponse().getContentAsString());
+        Mockito.verify(knockService, Mockito.times(1)).find(Mockito.argThat(matcher));
+    }
+
+    @Test
     public void testAdd() throws Exception {
         Knock input = new Knock();
         Mockito.doNothing().when(knockService).add(ArgumentMatchers.any(Knock.class));
@@ -42,5 +66,29 @@ public class KnockRestControllerTest {
                 .content(UnitTestUtil.entity2JsonText(new Knock())));
 
         Mockito.verify(knockService, Mockito.times(1)).add(ArgumentMatchers.any(Knock.class));
+    }
+
+    @Test
+    public void testSet() throws Exception {
+        Knock input = new Knock();
+        Mockito.doNothing().when(knockService).set(ArgumentMatchers.any(Knock.class));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .patch("/knock")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(UnitTestUtil.entity2JsonText(new Knock())));
+
+        Mockito.verify(knockService, Mockito.times(1)).set(ArgumentMatchers.any(Knock.class));
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+        int input = 1;
+        Mockito.doNothing().when(knockService).remove(1);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/knock/1/"));
+
+        Mockito.verify(knockService, Mockito.times(1)).remove(input);
     }
 }
