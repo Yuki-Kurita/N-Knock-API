672651af0a730fc19f5d38a21fdd35a1cdcae499
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-06-17 09:59:18 +0900

    Knock APIのService実装

--- src/test/java/com/good/tyotto/nknock/api/service/KnockServiceImplTest.java
+++ src/test/java/com/good/tyotto/nknock/api/service/KnockServiceImplTest.java
@@ -1,7 +1,10 @@
 package com.good.tyotto.nknock.api.service;
 
 import com.good.tyotto.nknock.api.domain.Knock;
+import com.good.tyotto.nknock.api.domain.KnockGroup;
+import com.good.tyotto.nknock.api.domain.KnockSelector;
 import com.good.tyotto.nknock.api.repository.KnockRepository;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -22,12 +25,37 @@ public class KnockServiceImplTest {
         }
 
         @Test
+        public void testFind() {
+            KnockSelector selector = new KnockSelector();
+            KnockServiceImpl target = new KnockServiceImpl(knockRepository);
+
+            target.find(selector);
+            Mockito.verify(knockRepository, Mockito.times(1)).find(selector);
+        }
+
+        @Test
         public void testAdd() {
             Knock input = new Knock();
             KnockServiceImpl target = new KnockServiceImpl(knockRepository);
 
             target.add(input);
             Mockito.verify(knockRepository, Mockito.times(1)).add(input);
+        }
+
+        @Test void testSet() {
+            Knock input = new Knock();
+            KnockServiceImpl target = new KnockServiceImpl(knockRepository);
+
+            target.set(input);
+            Mockito.verify(knockRepository, Mockito.times(1)).set(input);
+        }
+
+        @Test void testRemove() {
+            int input = 1;
+            KnockServiceImpl target = new KnockServiceImpl(knockRepository);
+
+            target.remove(input);
+            Mockito.verify(knockRepository, Mockito.times(1)).remove(input);
         }
     }
 }
